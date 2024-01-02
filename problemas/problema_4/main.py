4ffa42b56ccfdfdb3efb2417f9cedc478335fff6
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-02 07:47:09 -0300

    Função que devolve uma chave com probabilidade proporcional à frequência

---
+++ problemas/problema_4/main.py
@@ -0,0 +1,16 @@
+# Exercício 13.5 do livro "Pense em Python" do Allen B. Downey
+# Função que a partir de um histograma, devolva uma chave com probabilidade proporcional à sua frequência
+
+import random
+d = {'a': 2, 'b': 1}            # Um histograma qualquer
+
+
+def choose_from_hist(d):
+    lista = []
+    for item in d:
+        lista += item*d[item]
+    return random.choice(lista)
+# A ideia foi criar uma lista onde cada chave aparece "valor" vezes e usar o random.choice
+
+
+print(choose_from_hist(d))
