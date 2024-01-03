727f56ea49f2cb5060aee9ad44e29f1a6d0a1b85
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-03 14:08:10 -0300

    Problema da Contagem de Algarismos do NepsAcademy

--- problemas/problema_12/main.py
+++ problemas/problema_12/main.py
@@ -4,10 +4,7 @@ n = int(input())
 d = {}
 
 for numeral in '0123456789':
-    if numeral not in d:
-        d[numeral] = 0
-    else:
-        d[numeral] += 1
+    d[numeral] = 0
 
 for i in range(n):
     ni = str(input())
