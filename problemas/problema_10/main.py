7a94899a16d60b659ab9805b68014b5e77c3f47c
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-03 10:04:41 -0300

    Problema das lâmpadas do NepsAcademy

---
+++ problemas/problema_10/main.py
@@ -0,0 +1,23 @@
+# Problema "https://neps.academy/br/exercise/52"
+
+n = int(input())
+l = input().split()
+p2 = 0
+
+for i in range(n):
+    oi = int(l[i])
+    if oi == 2:
+        p2 += 1
+
+if n % 2 == 0:
+    r1 = 0
+else:
+    r1 = 1
+
+if p2 % 2 == 0:
+    r2 = 0
+else:
+    r2 = 1
+
+print(r1)
+print(r2)
