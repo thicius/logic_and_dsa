53a754a7c7e4aebdae431301bbbb8dc080c608bb
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-04 19:44:04 -0300

    Problema Basquete dos Robôs do NepsAcademy

---
+++ problemas/problema_041/main.py
@@ -0,0 +1,11 @@
+# Problema Basquete de Robôs do NepsAcademy
+# Link do Problema: https://neps.academy/br/exercise/162
+
+inp = int(input())
+
+if inp <= 800:
+    print(1)
+elif inp <= 1400:
+    print(2)
+elif inp <= 2000:
+    print(3)
