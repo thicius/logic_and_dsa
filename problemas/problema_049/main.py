e364b6619484b7f8c197b95d8fdfeb9d5c1a3a6c
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-05 21:11:55 -0300

    Dois problemas do NepsAcademy, 1 fácil e 1 difícil

---
+++ problemas/problema_049/main.py
@@ -0,0 +1,11 @@
+# Problema "A Idade da Dona Mônica" do Neps Academy
+# Link: https://neps.academy/br/exercise/463
+
+m = int(input())
+
+a = int(input())
+b = int(input())
+
+c = m - a - b
+
+print(max(a, b, c))
