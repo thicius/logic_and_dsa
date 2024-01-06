2831c170f4126d7d330a24fac635929596b041a7
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-06 07:19:31 -0300

    Função que 'subtrai' um dicionário de outro

--- problemas/problema_005/main.py
+++ problemas/problema_005/main.py
@@ -1,3 +1,6 @@
+# Esta função recebe um texto em formato de string (d1) junto com uma lista de palavras d2
+# E retorna em um dicionário as palavras de d1 que não estão em d2, com valor = none
+
 import string
 
 
@@ -9,8 +12,7 @@ def subtrair(d1, d2):
             res[word] = None
     return print(res)
 
-# Esta função recebe um texto em formato de string (d1) junto com uma lista de palavras d2
-# E retorna em um dicionário as palavras de d1 que não estão em d2, com valor = none
+# Exemplo:
 
 
 d1 = 'Minha terra tem palmeiras onde canta o sabiá; as aves, que aqui gorjeiam, não gorjeiam como lá.'
