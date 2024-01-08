595a1976717bdff32c92c3dd0326a2111faca531
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-08 09:04:27 -0300

    Problema Xadrez do NepsAcademy

---
+++ problemas/problema_052/main.py
@@ -0,0 +1,10 @@
+# Problema Xadrez do NepsAcademy
+# Link do Problema: https://neps.academy/br/exercise/164
+
+a = int(input())
+b = int(input())
+
+if (a - b) % 2 == 0:
+    print('1')
+else:
+    print('0')
