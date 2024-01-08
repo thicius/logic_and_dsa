42700ec5cc9557b22b2e104724a56848f613eb4d
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-08 09:11:23 -0300

    Problema Piso da Escola do NepsAcademy

---
+++ problemas/problema_053/main.py
@@ -0,0 +1,11 @@
+# Problema "Piso da Escola" do NepsAcademy
+# Link: https://neps.academy/br/exercise/167
+
+l = int(input())
+c = int(input())
+
+first = 2*l*c - l - c + 1
+second = 2*(l + c) - 4
+
+print(first)
+print(second)
