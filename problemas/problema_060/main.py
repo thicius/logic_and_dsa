6687c1f34411f9210717d9d57993a4c3e6b35b06
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-09 15:27:26 -0300

    Problema Plantação de Morangos

---
+++ problemas/problema_060/main.py
@@ -0,0 +1,12 @@
+# Problema "Platação de Morangos" do NepsAcademy
+# Link: https://neps.academy/br/exercise/53
+
+a = int(input())
+b = int(input())
+m = int(input())
+n = int(input())
+
+a1 = a*b
+a2 = m*n
+
+print(max(a1, a2))
