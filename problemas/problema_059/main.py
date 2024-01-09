a9c8339715bcea1082a347be061070884cd836f9
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-09 15:23:44 -0300

    Problema Ordenação de Três Números

---
+++ problemas/problema_059/main.py
@@ -0,0 +1,11 @@
+# Problema Ordenação de Três Números
+# Link: https://neps.academy/br/exercise/151
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+l = sorted([a, b, c])
+
+for item in l:
+    print(item)
