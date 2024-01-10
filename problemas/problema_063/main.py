02c6b79ef6652feb094385fd15572fa11ae433f2
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-10 07:42:56 -0300

    Problema Maior Área do NepsAcademy

--- problemas/problema_063/main.py
+++ problemas/problema_063/main.py
@@ -1,15 +1,16 @@
-x = int(input())
-k = 0
+# Problema "Maior Área" do NepsAcademy
+# https://neps.academy/br/exercise/214
 
+line1 = input().split()
+line2 = input().split()
 
-def collatz(x):
-    if x == 1:
-        return 's'
-    else:
-        if x % 2 == 0:
-            collatz(int(x/2))
-        else:
-            collatz(3*x + 1)
+prod1 = int(line1[0])*int(line1[1])
 
+prod2 = int(line2[0])*int(line2[1])
 
-print(collatz(x))
+if prod1 > prod2:
+    print('Primeiro')
+elif prod1 < prod2:
+    print('Segundo')
+else:
+    print('Empate')
