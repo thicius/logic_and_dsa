ddab93221df42e1cd33b7ddce31fb37055c3af0b
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-11 12:10:27 -0300

    Problema Gangorra do NepsAcademy

--- problemas/problema_016/main.py
+++ problemas/problema_016/main.py
@@ -1,9 +1,8 @@
-# Exercício "https://neps.academy/br/course/programacao-basica-(codcad)/lesson/gangorra"
+# Exercício Gangorra do Neps Academy
+# Link: "https://neps.academy/br/course/programacao-basica-(codcad)/lesson/gangorra"
 
-# Lendo a entrada do exercício
 P1, C1, P2, C2 = map(int, input().split())
 
-# Seu código vai aqui
 if P1*C1 == P2*C2:
     print(0)
 elif P1*C1 > P2*C2:
