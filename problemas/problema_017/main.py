6f66d2477998c623d3fe0798f6796688b38a62db
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-11 12:10:50 -0300

    Problema Par ou Impar do NepsAcademy

--- problemas/problema_017/main.py
+++ problemas/problema_017/main.py
@@ -1,10 +1,9 @@
-# Exercício "https://neps.academy/br/course/programacao-basica-(codcad)/lesson/par-ou-impar"
+# Exercício "Par ou Ímpar" do Neps Academy
+# Link: "https://neps.academy/br/course/programacao-basica-(codcad)/lesson/par-ou-impar"
 
-# Lendo a entrada do exercício
 b = int(input())
 c = int(input())
 
-# Seu código vai aqui
 soma = b + c
 if float(soma//2) == float(soma/2):
     print('Bino')
