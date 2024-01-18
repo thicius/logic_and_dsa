57d7af6ba0726c305dd5c43eebc873486c59e9db
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-18 07:12:26 -0300

    Problema Nota Esquecida do NepsAcademy

---
+++ problemas/problema_090/main.py
@@ -0,0 +1,10 @@
+# Problema Nota Esquecida, NepsAcademy
+# Link: https://neps.academy/br/exercise/470
+
+a = int(input())
+r = int(input())
+
+# como sabemos que a + b = 2*r
+
+b = 2*r - a
+print(b)
