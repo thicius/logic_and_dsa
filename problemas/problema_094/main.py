4c2dd564e7c3f39f18084026bc915587d480c077
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-19 08:24:57 -0300

    Problema Lançamento de Dados, NepsAcademy

---
+++ problemas/problema_094/main.py
@@ -0,0 +1,29 @@
+# Problema Lançamento de Dados do Neps Academy
+# Link: https://neps.academy/br/exercise/188
+
+n = int(input())
+l = []
+
+for i in range(12):
+    l += [0]
+
+faces = []
+
+for i in range(n):
+    faces += [int(input())]
+
+maximos = []
+
+for numero in faces:
+    l[numero - 1] += 1
+
+
+for i in range(12):
+    if l[i] == max(l):
+        maximos += [i+1]
+
+s = ''
+for valores in maximos:
+    s += str(valores) + ' '
+
+print(s)
