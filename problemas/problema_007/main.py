9d61058fbb6c3000917d01314be3c2498f36f367
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-23 19:48:49 -0300

    Exercício 9.5 do livro PenseEmPython

--- problemas/problema_007/main.py
+++ problemas/problema_007/main.py
@@ -1,17 +1,10 @@
-# Exercício 9.5 do livro Pense em Python de Allen B. Downey
+# Problema Idade de Camila do NepsAcademy
+# Link: https://neps.academy/br/exercise/1479
 
-def uses_all(palavra, letras_obrigatorias):
-    n = 0
-    for letra in letras_obrigatorias:
-        if letra in palavra:
-            n += 1
-    if n == len(letras_obrigatorias):
-        return True
-    else:
-        return False
+f = int(input())
+s = int(input())
+t = int(input())
 
-# Esta função recebe uma palavra e uma string, que se suas letras estiverem todas na palavra, retorna True
+l = sorted([f, s, t])
 
-
-print(uses_all('Amazing', 'aing'))
-print(uses_all('Amazing', 'oing'))
+print(l[1])
