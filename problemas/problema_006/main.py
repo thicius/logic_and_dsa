3f16527cb8a4a718e8d0ef2a26291169d5ba656f
Thiago Vinicius <thiagoviniciusalvesdeoliveira@gmail.com>
2024-01-30 20:30:07 -0300

    Problema Nota Esquecida, NepsAcademy

--- problemas/problema_006/main.py
+++ problemas/problema_006/main.py
@@ -1,26 +1,10 @@
-# Exercício 9.3 do livro Pense em Python, de Allen B. Downey
+# Problema Nota Esquecida, NepsAcademy
+# Link: https://neps.academy/br/exercise/470
 
-def avoids(palavra, letras_proibidas):
-    for letra in letras_proibidas:
-        if letra in palavra:
-            return False
-    return True
+a = int(input())
+r = int(input())
 
-# Esta função recebe uma palavra e uma string de "letras proibidas" que retornaram False se estiverem na palavra
+# como sabemos que a + b = 2*r
 
-
-print(avoids('Python', 'yn'))
-print(avoids('Python', 'asdfg'))
-
-# A próxima função usa um arquivo em txt contendo palavras, que será chamado words.txt
-
-
-def avoids_(letras_proibidas):
-    n = 0
-    for palavra in open('words.txt'):
-        if avoids(palavra, letras_proibidas) == True:
-            n += 1
-    return n
-
-# A função acima, depende da função anterior e do arquivo contendo as palavras
-# Recebe uma string contendo certas letras, e devolve o número de palavras em 'words.txt' que não as usam
+b = 2*r - a
+print(b)
